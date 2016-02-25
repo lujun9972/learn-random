f9e6345d37a8ef40660bb5af92508a81bc2089b2
darksun <lujun9972@sina.com>
2016-02-25 15:17:57 +0800

    add learn-random-variable

--- learn-random-function.el
+++ learn-random-function.el
@@ -1,31 +1,47 @@
-(defvar learn-random-functions nil
+(defvar learn-random/functions nil
   "functions to be learn")
 
-(defun learn-random-function-update ()
+(defun learn-random/filter-function (symbol)
+  (when (and (functionp symbol)
+             (not (get symbol 'byte-obsolete-info))
+             (documentation symbol)
+             (not (string-prefix-p "\n" (documentation symbol))))
+    (push symbol learn-random/functions)))
+
+(defun learn-random/update-function ()
   (interactive)
-  (setq learn-random-functions nil)
-  (mapatoms (lambda (symbol)
-              (when (and (functionp symbol)
-                         (get symbol 'byte-obsolete-info)
-                         (documentation symbol))
-                (push symbol learn-random-functions)))))
+  (setq learn-random/functions nil)
+  (mapatoms #'learn-random/filter-function))
 
 (defun learn-random-function ()
   (interactive)
-  (unless learn-random-functions
-    (learn-random-function-update))
-  (let* ((limit (length learn-random-functions))
+  (unless learn-random/functions
+    (learn-random/update-function))
+  (let* ((limit (length learn-random/functions))
          (idx (random limit))
-         (function-symbol (nth idx learn-random-functions)))
+         (function-symbol (nth idx learn-random/functions)))
     (describe-function function-symbol)))
 
-;; (get 'describe-function 'function-documentation)
 
-;; (get 'string-to-int 'function-documentation)
-;; (get-)
-;; (get  'defun 'byte-obsolete-info)
+(defvar learn-random/variables nil
+  "variables to be learn")
 
-;; (get 'string-to-int 'byte-obsolete-info)
+(defun learn-random/filter-variable (symbol)
+  (when (and (boundp symbol)
+             (not (get symbol 'byte-obsolete-info))
+             (documentation-property symbol 'variable-documentation))
+    (push symbol learn-random/variables)))
 
-;; (documentation 'auto-pause-pause-process)
-;; (symbol-plist 'auto-pause-pause-process)
+(defun learn-random/update-variable ()
+  (interactive)
+  (setq learn-random/variables nil)
+  (mapatoms #'learn-random/filter-variable))
+
+(defun learn-random-variable ()
+  (interactive)
+  (unless learn-random/variables
+    (learn-random/update-variable))
+  (let* ((limit (length learn-random/variables))
+         (idx (random limit))
+         (variable-symbol (nth idx learn-random/variables)))
+    (describe-variable variable-symbol)))
