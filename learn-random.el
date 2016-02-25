30148c35701258176f9c4a93886344f2d81c6d53
darksun <lujun9972@sina.com>
2016-02-25 15:33:54 +0800

    过滤说明为空的变量

--- learn-random.el
+++ learn-random.el
@@ -13,6 +13,7 @@
   (setq learn-random/functions nil)
   (mapatoms #'learn-random/filter-function))
 
+;;;###autoload
 (defun learn-random-function ()
   (interactive)
   (unless learn-random/functions
@@ -29,14 +30,18 @@
 (defun learn-random/filter-variable (symbol)
   (when (and (boundp symbol)
              (not (get symbol 'byte-obsolete-info))
-             (documentation-property symbol 'variable-documentation))
+             (documentation-property symbol 'variable-documentation)
+             (not (string= "" (documentation-property symbol 'variable-documentation))))
     (push symbol learn-random/variables)))
+
+(documentation-property 'ediff-highlighting-style 'variable-documentation)
 
 (defun learn-random/update-variable ()
   (interactive)
   (setq learn-random/variables nil)
   (mapatoms #'learn-random/filter-variable))
 
+;;;###autoload
 (defun learn-random-variable ()
   (interactive)
   (unless learn-random/variables
@@ -45,3 +50,4 @@
          (idx (random limit))
          (variable-symbol (nth idx learn-random/variables)))
     (describe-variable variable-symbol)))
+(provide 'learn-random)
